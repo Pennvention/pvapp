47eddaeb402583c984a3092e02dc97582e2f354a
nkav <nakavthekar@gmail.com>
2014-01-18 18:15:38 -0500

    placed db.init_app() in __init__.py

--- manage.py
+++ manage.py
@@ -3,9 +3,7 @@ from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 
 migrate = Migrate(app, db)
-
 manager = Manager(app)
-
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__': 
